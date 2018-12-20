b47fefefdc26b6a8d8568370309d18ef672d77f2
Kamel Boukhateb <kamel.boukhateb@prestashop.com>
2018-12-20 16:06:04 +0100

    Bump version 1.1.2

--- psgdpr.php
+++ psgdpr.php
@@ -81,7 +81,7 @@ class Psgdpr extends Module
         // Settings
         $this->name = 'psgdpr';
         $this->tab = 'administration';
-        $this->version = '1.1.1';
+        $this->version = '1.1.2';
         $this->author = 'PrestaShop';
         $this->need_instance = 0;
 
