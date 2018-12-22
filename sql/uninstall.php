437b4e8d7cb721410f3ea318f933fd14a64ae017
MathiasReker <mathias@reker.dk>
2018-12-22 17:45:19 +0100

    phpdoc indent

--- sql/uninstall.php
+++ sql/uninstall.php
@@ -25,10 +25,10 @@
 */
 
 /**
-* In some cases you should not drop the tables.
-* Maybe the merchant will just try to reset the module
-* but does not want to loose all of the data associated to the module.
-*/
+ * In some cases you should not drop the tables.
+ * Maybe the merchant will just try to reset the module
+ * but does not want to loose all of the data associated to the module.
+ */
 
 $sql = array();
 
